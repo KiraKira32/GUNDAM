7401875dba9ef713e767c1a575ea0fc30358af83
KiraKira32 <chiennnk32@gmail.com>
2023-05-15 21:42:40 +0800

    購物車頁面

--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -2,6 +2,23 @@
   <div>
     我是購物車
   </div>
+        </div>
+      </div> -->
+      <!-- 購物車列表 -->
+
+      <div class="cart-text p-5 mx-5">
+        <h3 class="mb-4 text-center">購物車</h3>
+          <div class="shopList p-2">
+            <div class="text-end d-flex justify-content-end  align-items-center">
+              <IconTrash color="#DF0000" size="20"/>
+              <span class="text-danger">
+                清空購物車</span>
+            </div>
+            <hr class="border-line">
+          </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style>
