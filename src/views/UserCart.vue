325948c0e77e92269cbc49c9276df7ef468f1930
KiraKira32 <chiennnk32@gmail.com>
2023-05-17 20:48:52 +0800

    購物車頁面撰寫

--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -21,54 +21,102 @@
     </div>
     <div class="container container-block shadow-sm p-0 mb-4">
       <!-- 購物車 沒有產品 -->
-
       <!-- <div class="cart-text text-center p-5">
         <h3 class="mb-4">購物車</h3>
-        <div class="cart-block py-4">
+        <div class="cart-block py-5">
           <div class="fs-5 mb-4">您尚未選購任何商品，請先挑選您喜愛的商品。</div>
           <div>
-            <router-link :to="`/products`" class="btn btn-danger">馬上來看看</router-link>
+            <router-link :to="`/products`" class="btn btn-danger">前往選購</router-link>
           </div>
         </div>
       </div> -->
+
       <!-- 購物車列表 -->
 
       <div class="cart-text p-5 mx-5">
         <h3 class="mb-4 text-center">購物車</h3>
-          <div class="shopList p-2">
-            <div class="text-end d-flex justify-content-end  align-items-center">
-              <IconTrash color="#DF0000" size="20"/>
-              <a href="" class="text-decoration-none">
-                <span class="text-danger">清空購物車</span>
-              </a>
-            </div>
-            <hr class="border-line">
-            <div class="d-flex">
-              <div>產品圖片</div>
-              <div>產品名稱</div>
-              <div>購買數量</div>
-              <div>價格</div>
-              <div>x</div>
-            </div>
+        <div class="shopList p-2">
+          <div class="text-end d-flex justify-content-end align-items-center">
+            <IconTrash color="#DF0000" size="20" />
+            <a href="" class="text-decoration-none">
+              <span class="text-danger">清空購物車</span>
+            </a>
           </div>
-          <div>
-            <div class="d-flex justify-content-end ">
-              <div class="products-num col-2">總計</div>
-              <div class="products-price"> NT$9999</div>
-            </div>
+          <hr class="border-line" />
+
+          <table class="table align-middle">
+            <tbody>
+              <tr v-for="item in cartData.carts" :key="item.id" class="">
+                <td>
+                  <img
+                    class="carts-img"
+                    :src="item.product.imageUrl"
+                    alt="item.product.imageUrl"
+                    @click="$router.push(`/product/${item.product_id}`)"
+                  />
+                </td>
+                <td>{{ item.product.title }}</td>
+                <td style="width: 80px;">
+                  <div class="input-group input-group-sm" style="width: 80px">
+                    <div class="input-group">
+                      <input
+                        min="1"
+                        type="number"
+                        class="form-control text-center"
+                        v-model.number="item.qty"
+                        @change="updataCart(item)"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td class="text-danger text-right">
+                  NT$ {{ $filters.currency(item.product.price) }}
+                </td>
+                <td class="align-middle text-right px-0">
+                  {{ $filters.currency(item.product.price * item.qty) }}
+                </td>
+                <td class="text-right"><IconX color="#DF0000" /></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="total-block d-flex justify-content-end py-4">
+          <div v-if="cartData.carts" class="text-secondary mx-4">
+            共 {{ cartData.carts.length }} 件商品
           </div>
+          <div class="products-num text-end text-secondary">商品總金額</div>
+          <div class="products-price mx-4 text-danger">
+            NT${{ $filters.currency(cartData.total) }}
+          </div>
+        </div>
+        <div class="d-flex justify-content-between mt-5">
+          <button
+            type="button"
+            class="total-btn btn btn-color text-white py-2"
+            @click="$router.push(`/products`)"
+          >
+            繼續購物
+          </button>
+          <button type="button" class="total-btn btn btn-danger py-2">前往結帳</button>
+        </div>
       </div>
     </div>
   </main>
+  <ToastMessages :title="title" :message="message"></ToastMessages>
 </template>
 
 <script>
+import { IconTrash } from '@tabler/icons-vue'
+import { IconX } from '@tabler/icons-vue'
+import { Toast } from 'bootstrap'
 
-import { IconTrash } from '@tabler/icons-vue';
+import ToastMessages from '@/components/ToastMessages.vue'
 
 export default {
   components: {
-    IconTrash
+    IconTrash,
+    IconX,
+    ToastMessages
   },
   data() {
     return {
@@ -77,24 +125,51 @@ export default {
       productId: '',
       cartData: {},
       cartStatus: false,
+      showToast: false,
+      title: '',
+      message: ''
     }
   },
   methods: {
     // 取得購物車資料
     getCart() {
+      // this.isLoading = true
       const url = `${import.meta.env.VITE_API}/v2/api/${import.meta.env.VITE_PATH}/cart`
       this.$http
         .get(url)
         .then((res) => {
+          this.isLoading = false
           this.cartData = res.data.data
+          console.log(this.cartData)
         })
         .catch((err) => {
           alert(err.data.message)
         })
     },
-    updataCart() {
-
-    }
+    // 調整購物車數量
+    updataCart(item) {
+      const url = `${import.meta.env.VITE_API}/v2/api/${import.meta.env.VITE_PATH}/cart/${item.id}`
+      const data = {
+        product_id: item.product.id,
+        qty: item.qty
+      }
+      this.$http.put(url, { data }).then(() => {
+        this.getCart()
+        this.toastShow('購物車訊息', '商品數量更新成功', 2000)
+      })
+    },
+    toastShow (title, message, duration) {
+        this.title = title
+        this.message = message
+        this.toastMessage.show()      
+        setTimeout(() => {
+          this.toastMessage.hide();
+        }, duration);
+      },
+  },
+  mounted() {
+    this.getCart()
+    this.toastMessage = new Toast('#toastMessage')
   }
 }
 </script>
